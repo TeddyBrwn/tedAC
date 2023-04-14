323ff4e0ce848e46597fa3482a54823400bc6984
teddy <huynhdong1115@gmail.com>
2023-04-14 13:42:52 +0700

    done SideBar&LackData

--- src/components/Layout/DefaultLayout/Sidebar/index.js
+++ src/components/Layout/DefaultLayout/Sidebar/index.js
@@ -8,12 +8,18 @@ function Sidebar() {
     <aside className={cx("wrapper")}>
       <div className={cx("inner")}>
         <div className={cx("header-list")}>
-          <span>List</span>
-        </div>
-        <div className={cx("product")}>
-          <div className={cx("a")}>a</div>
-          <div className={cx("b")}>b</div>
-          <div className={cx("c")}>c</div>
+          <span className={cx("list")}>List</span>
+          <div className={cx("product")}>
+            <div className={cx("a")}>a</div>
+            <div className={cx("b")}>b</div>
+            <div className={cx("d")}>c</div>
+            <div className={cx("e")}>e</div>
+            <div className={cx("f")}>f</div>
+            <div className={cx("g")}>g</div>
+            <div className={cx("h")}>h</div>
+            <div className={cx("k")}>k</div>
+            <div className={cx("l")}>l</div>
+          </div>
         </div>
       </div>
     </aside>
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,17 @@
-function Home() {
-  return <h2>Home page</h2>;
-}
+import classNames from "classnames/bind";
+import styles from "./Home.module.scss";
 
+const cx = classNames.bind(styles);
+
+function Home() {
+  return (
+    <div className={cx("wrapper")}>
+      <div className={cx("inner")}>
+        <div className={cx("content")}>
+          <span>header</span>
+        </div>
+      </div>
+    </div>
+  );
+}
 export default Home;
