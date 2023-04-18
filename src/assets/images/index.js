6444d7d7e627f0631156835f2a580c63b6b091a2
teddy <huynhdong1115@gmail.com>
2023-04-18 08:31:59 +0700

    done aside

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,5 +1,5 @@
 const images = {
-  logo: require("~/assets/images/logoted.svg").default,
+  logo: require("~/assets/images/logo.svg").default,
 };
 
 export default images;
--- src/components/Layout/DefaultLayout/Sidebar/index.js
+++ src/components/Layout/DefaultLayout/Sidebar/index.js
@@ -1,5 +1,20 @@
 import classNames from "classnames/bind";
 import styles from "./Sidebar.module.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faArrowRightArrowLeft,
+  faChartColumn,
+  faList,
+  faRightFromBracket,
+  faSliders,
+  faUserCheck,
+} from "@fortawesome/free-solid-svg-icons";
+import {
+  faObjectGroup,
+  faHeart,
+  faComment,
+  faFileLines,
+} from "@fortawesome/free-regular-svg-icons";
 
 const cx = classNames.bind(styles);
 
@@ -7,18 +22,49 @@ function Sidebar() {
   return (
     <aside className={cx("wrapper")}>
       <div className={cx("inner")}>
-        <div className={cx("header-list")}>
-          <span className={cx("list")}>List</span>
-          <div className={cx("product")}>
-            <div className={cx("a")}>a</div>
-            <div className={cx("b")}>b</div>
-            <div className={cx("d")}>c</div>
-            <div className={cx("e")}>e</div>
-            <div className={cx("f")}>f</div>
-            <div className={cx("g")}>g</div>
-            <div className={cx("h")}>h</div>
-            <div className={cx("k")}>k</div>
-            <div className={cx("l")}>l</div>
+        <div className={cx("list")}>
+          <div className={cx("list_top_left")}>
+            <div className={cx("Dasboard")}>
+              <FontAwesomeIcon icon={faChartColumn} />
+              <span>Dasboard</span>
+            </div>
+            <div className={cx("Discover")}>
+              <FontAwesomeIcon icon={faList} />
+              <span>Discover</span>
+            </div>
+            <div className={cx("Tickets")}>
+              <FontAwesomeIcon icon={faFileLines} />
+              <span>Tickets</span>
+            </div>
+            <div className={cx("Favorite")}>
+              <FontAwesomeIcon icon={faHeart} />
+              <span>Favorite</span>
+            </div>
+            <div className={cx("Messages")}>
+              <FontAwesomeIcon icon={faComment} />
+              <span>Messages</span>
+            </div>
+            <div className={cx("Transaction")}>
+              <FontAwesomeIcon icon={faArrowRightArrowLeft} />
+              <span>Transaction</span>
+            </div>
+          </div>
+          <div className={cx("Discount")}>
+            <h2>50% Discount!</h2>
+            <span className={cx("Discount-text")}>
+              Get best discount on certain days and don't miss it
+            </span>
+            <button className={cx("Discount-btn")}>Explore Now !!</button>
+          </div>
+          <div className={cx("list_bottom_left")}>
+            <div className={cx("Settings")}>
+              <FontAwesomeIcon icon={faSliders} />
+              <span>Settings</span>
+            </div>
+            <div className={cx("Log out")}>
+              <FontAwesomeIcon icon={faRightFromBracket} />
+              <span>Log out</span>
+            </div>
           </div>
         </div>
       </div>
